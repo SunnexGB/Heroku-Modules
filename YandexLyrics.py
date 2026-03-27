dee8cd13f16fa7f0405c391c64700b1223bbba74
SunnexGB <sibircevtt@gmail.com>
2026-03-27 14:50:26 +0300

    re-write module SpotifyLyrics to YandexLyrics

--- YandexLyrics.py
+++ YandexLyrics.py
@@ -1,8 +1,8 @@
 # meta developer: @SunnexGB
 # requires: aiohttp
-# meta pic: https://r2.fakecrime.bio/uploads/f49a9294-36ad-4fc4-801f-48cb049111d6.jpg
-# meta banner: https://r2.fakecrime.bio/uploads/f49a9294-36ad-4fc4-801f-48cb049111d6.jpg
-# meta fhsdesc: Spotify, music, музыка, спотифай,Lyrics, слова, текст, трек, песня
+# meta pic: https://r2.fakecrime.bio/uploads/ab42b5e2-91f1-4ed1-8002-51b3184e3839.jpg
+# meta banner: https://r2.fakecrime.bio/uploads/ab42b5e2-91f1-4ed1-8002-51b3184e3839.jpg
+# meta fhsdesc: YaMusic, music, музыка, яндекс музыка,Lyrics, слова, текст, трек, песня
 # все же я не знаю трек или сонг, так что пусть будет трек, а не сонг потому что интуитивнее поняттнее,наверное?
 # крутой баннер да?
 #current version
@@ -16,13 +16,13 @@ import re
 
 
 @loader.tds
-class SpotifyLyrics(loader.Module):
+class YandexLyrics(loader.Module):
     """life lyrics current song"""
 
     strings = {
-        "name": "SpotifyLyrics",
-        "no_spotifymod": "<tg-emoji emoji-id=5431402435497181911>💢</tg-emoji> <b>SpotifyMod not found.</b>",
-        "no_spotify": "<tg-emoji emoji-id=5429164207780152924>😅</tg-emoji> <b>Nothing is playing on Spotify.</b>",
+        "name": "YandexLyrics",
+        "no_YaMusicMod": "<tg-emoji emoji-id=5431402435497181911>💢</tg-emoji> <b>YaMusicMod not found.</b>",
+        "no_ym": "<tg-emoji emoji-id=5429164207780152924>😅</tg-emoji> <b>Nothing is playing on YaMusic.</b>",
         "no_lyrics": "<tg-emoji emoji-id=5431402435497181911>💢</tg-emoji> <b>Lyrics not found for:</b> <code>{}</code>",
         "not_synced": "<i><tg-emoji emoji-id=5431445849026611010>⚠️</tg-emoji> Lyrics are not synchronized.</i>\n\n",
         "finished": "<tg-emoji emoji-id=5429638011392377649>‼️</tg-emoji> Playback ended or track changed.",
@@ -32,8 +32,8 @@ class SpotifyLyrics(loader.Module):
 
     strings_ru = {
         "cls_doc": "Лайв слова текущей песни.",
-        "no_spotifymod": "<tg-emoji emoji-id=5431402435497181911>💢</tg-emoji> <b>SpotifyMod не найден.</b>",
-        "no_spotify": "<tg-emoji emoji-id=5429164207780152924>😅</tg-emoji> <b>В Spotify ничего не играет.</b>",
+        "no_YaMusicMod": "<tg-emoji emoji-id=5431402435497181911>💢</tg-emoji> <b>YaMusic не найден.</b>",
+        "no_ym": "<tg-emoji emoji-id=5429164207780152924>😅</tg-emoji> <b>В YaMusic ничего не играет.</b>",
         "no_lyrics": "<tg-emoji emoji-id=5431402435497181911>💢</tg-emoji> <b>Текст не найден для:</b> <code>{}</code>",
         "not_synced": "<i><tg-emoji emoji-id=5431445849026611010>⚠️</tg-emoji> Текст не синхронизирован.</i>\n\n",
         "finished": "<tg-emoji emoji-id=5429638011392377649>‼️</tg-emoji> Воспроизведение завершено или трек сменился.",
@@ -54,7 +54,7 @@ class SpotifyLyrics(loader.Module):
                 "lyrics_delay",
                 0.5,
                 "delay in switching to a new timing sector with words",
-                validator=loader.validators.Float(minimum=0.1),
+                validator=loader.validators.Float(minimum=0.5),
             ),
         )
 
@@ -131,8 +131,8 @@ class SpotifyLyrics(loader.Module):
         last_display = ""
         try:
             while True:
-                pb = mod.sp.current_playback()
-                if not pb or not pb.get("item") or pb["item"]["id"] != track_id:
+                pb = await mod._YaMusicMod__get_now_playing()
+                if not pb or not pb.get("track") or pb["track"]["track_id"] != track_id:
                     try:
                         await form.edit(
                             self.strings("finished"),
@@ -167,20 +167,20 @@ class SpotifyLyrics(loader.Module):
             self._active_tasks.pop(track_id, None)
 
     @loader.command(ru_doc="- показать синхронизированный текст песни")
-    async def snowlcmd(self, message: Message):
-        """- show synchronized lyrics for current Spotify track"""
-        mod = self.lookup("SpotifyMod")
-        if not mod or not hasattr(mod, "sp") or not mod.sp:
-            return await utils.answer(message, self.strings("no_spotifymod"))
+    async def ynowlcmd(self, message: Message):
+        """- show synchronized lyrics for current YaMusic track"""
+        mod = self.lookup("YaMusic")
+        if not mod:
+            return await utils.answer(message, self.strings("no_YaMusicMod"))
 
-        playback = mod.sp.current_playback()
-        if not playback or not playback.get("item"):
-            return await utils.answer(message, self.strings("no_spotify"))
+        playback = await mod._YaMusicMod__get_now_playing()
+        if not playback or not playback.get("track"):
+            return await utils.answer(message, self.strings("no_ym"))
 
-        track = playback["item"]
-        track_id = track["id"]
-        artist_name = track["artists"][0]["name"]
-        track_name = track["name"]
+        track = playback["track"]
+        track_id = track["track_id"]
+        artist_name = ", ".join(track["artist"])
+        track_name = track["title"]
         song_url = f"https://song.link/s/{track_id}"
 
         old = self._active_tasks.pop(track_id, None)
@@ -211,7 +211,7 @@ class SpotifyLyrics(loader.Module):
             reply_markup=self._markup(song_url),
         )
 
-        task = asyncio.ensure_future(
+        task = asyncio.create_task(
             self.run_loop(
                 form=form,
                 mod=mod,
@@ -225,3 +225,68 @@ class SpotifyLyrics(loader.Module):
             )
         )
         self._active_tasks[track_id] = task
+
+                # Fan-fantasizing
+                # Fa-fa-fa-fantasizing
+                # You and I-I-I
+                # When I close my eyes (my eyes)
+                # Nothings real
+                # Fantasizing (fantasizing)
+                # Bout you and I
+                # Cos you only hit my line
+                # When you wanna waste time
+                # I know you're so busy
+                # But trust me baby I'm not blind (blind)
+                # Uh oh, you and I
+                # We could never be
+                # Uh oh you and I
+                # Cos we will never be
+                # Uh oh, you and I
+                # No, we will never be
+                # That pretty picture that I painted in my mind (mind)
+                # So tell me what (tell me, tell me)
+                # The view is like
+                # With your head in the clouds
+                # And tell me what (tell me, tell me)
+                # It feels like to be right all the time
+                # You say that you love me
+                # But you don't even love yourself (no)
+                # Wanna get in my head
+                # But I ain't gonna let you close (no)
+                # Tryna control me
+                # But I ain't gon' play your game
+                # No more
+                # No I won't
+                # When I close (when I close)
+                # My eyes (my eyes)
+                # Nothing's real (no)
+                # Fantasizing (fantasize)
+                # bout you-you-you-you-you
+                # You-you-you-you
+                # You-you-you-you
+                # You-you-you
+                # And I
+                # You-you-you-you-you-you-you
+                # You-you-you-you-you-you-you
+                # You-you-you-you-you
+                # And I
+                # This is the last time I tell you
+                # Don't come round my way if you're just gon' waste my time
+                # And no, I won't be there for the long run
+                # No, not I
+                # But you never get (never get)
+                # The message, do you?
+                # You never seem (never seem)
+                # To grip an understanding
+                # That you emulate a ghost
+                # I pointed out all of your flaws
+                # But you still came up with excuses for em all
+                # So typical (so typical)
+                # You know it all
+                # So of course, I'm the one that's wrong (right?)
+                # When I close my eyes
+                # Nothings real
+                # Fan- fantasize
+                # Fa-fa-fa-fa
+                # Fantasizing
+                # Fa-fantasizing bout you and I
